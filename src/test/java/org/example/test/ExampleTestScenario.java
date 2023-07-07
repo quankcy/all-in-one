580276fd67a4b161f0551bf4322fcc2b7cc1ca2e
Karol Poznakowski <karol.poznakowski@gmail.com>
2023-07-07 16:27:33 +0200

    Patch ExampleTestScenario test case

--- src/test/java/org/example/test/ExampleTestScenario.java
+++ src/test/java/org/example/test/ExampleTestScenario.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 public class ExampleTestScenario {
 
     @Test
-    public void exampleTestCaseNamedMeaningfullyTestCase() {
+    public void exampleTestCaseNamedMeaningfully() {
         // GIVEN - prepare data here
         int valueA = 6;
         int valueB = 10;
 
         // WHEN - test subject
-        int result = maxMethod(6, 10);
+        int result = maxMethod(valueA, valueB);
 
         // THEN - test assertions
         Assertions.assertEquals(10, result);
